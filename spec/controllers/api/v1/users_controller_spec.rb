4be17f77b3d740e5c75602277cab062602ad79d6
Roman Gural <7.gural@gmail.com>
2015-02-13 15:40:52 +0100

    refactors test headers

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 describe Api::V1::UsersController, :type => :controller do
-  before (:each) { request.headers['Accept'] = "application/vnd.dobro.v1, #{Mime::JSON}" }
-  before (:each) { request.headers['Content-Type'] = Mime::JSON.to_s }
+  before (:each) {
+    request.headers['Accept'] = "application/vnd.dobro.v1, #{Mime::JSON}"
+    request.headers['Content-Type'] = Mime::JSON.to_s}
 
   describe "GET #show" do
     before(:each) do
